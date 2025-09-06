9fd40f25ee9a4c974985396a83a661fe0bacce3a
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-09-06 23:22:59 +0900

    CSS

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -4,22 +4,37 @@ import { styled } from "styled-components";
 export const Header = () => {
   return (
     <SHeader>
-      <p>人生</p>
+      <SNav>
+      <SLogo>人生</SLogo>
       <SLink to="/">入力</SLink>
       <SLink to="/Results">記録</SLink>
       <SLink to="/Graph">グラフ</SLink>
       <SLink to="/Rate">勝率</SLink>
+      </SNav>
     </SHeader>
   );
 };
 
 const SHeader = styled.header`
-  background-color: red;
-  text-align: center;
-  padding: 8px 0;
+  display: flex;
+  justify-content: space-between;
+  background-color: #0a2540;
+  align-items: center;
+  padding: 8px 0px;
+  color: white;
 `;
 
 const SLink = styled(Link)`
   margin: 20px;
   color: green;
+`;
+
+const SLogo = styled.p`
+font-size:20px;
+font-weight:bold
+margin:0;
+`;
+const SNav = styled.nav`
+  display: flex;
+  gap: 16px;
 `;
